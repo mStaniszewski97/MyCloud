83357f5b3e0c92c61ac899c09e3255b7c01552d1
MichalStaniszewski <m.staniszewski16@gmail.com>
2019-03-19 09:55:08 +0100

    init commit(Front)

---
+++ demo-ng-app/src/app/app.module.ts
@@ -0,0 +1,60 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { NotesComponent } from './notes/notes.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { Router, Routes, RouterModule} from '@angular/router';
+import { FormsModule} from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes : Routes = [
+  {
+    path:'notes',
+    component:NotesComponent
+  },
+  {
+    path:'feedback',
+    component:FeedbackComponent
+  },
+  {
+    path:'',
+    component:HomeComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'**',
+    component:NotFoundComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    FeedbackComponent,
+    NotesComponent,
+    NotFoundComponent,
+    HomeComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
